53e59d3dc6c99b7c2a8362978b7bf3feeb41cd78
Denis Moreira <silvad281@gmail.com>
2023-07-08 19:25:01 -0300

    feat: consumindo api (fictícia)

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,27 @@
 import { BsCartPlus } from "react-icons/bs";
+import { useEffect, useState } from "react";
+
+import { api } from "../../services/api";
+
+interface ProductsProps {
+   id: number;
+   title: string;
+   description: string;
+   price: number;
+   cover: string;
+}
 
 export function Home() {
+   const [products, setProducts] = useState<ProductsProps[]>([]);
+
+   useEffect(() => {
+      async function getProducts() {
+         const response = await api.get("/products");
+         setProducts(response.data);
+      }
+      getProducts();
+   }, []);
+
    return (
       <div>
          <main className="w-full max-w-7xl px-4 mx-auto">
@@ -8,20 +29,27 @@ export function Home() {
                Produtos em alta
             </h1>
             <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-               <section className="w-full">
-                  <img
-                     className="w-full rounded-lg max-h-70 mb-b"
-                     src="https://images.tcdn.com.br/img/img_prod/1077127/celular_xiaomi_redmi_9a_2gb_ram_dual_32gb_6_53_glacial_blue_16753_1_78f5104e6e8985618a33cdb1f312550a.jpeg"
-                     alt="celular xiaomi"
-                  />
-                  <p className="font-medium mt-1 mb-2  ">Celular</p>
-                  <div className="flex gap-3 items-center">
-                     <strong className="text-zinc-700/90">R$ 1000,00</strong>
-                     <button className="bg-zinc-900 p-1 rounded">
-                        <BsCartPlus size={28} color="#fff" />
-                     </button>
-                  </div>
-               </section>
+               {products.map((product) => (
+                  <section key={product.id} className="w-full">
+                     <img
+                        className="w-full rounded-lg max-h-70 mb-b"
+                        src={product.cover}
+                        alt={product.title}
+                     />
+                     <p className="font-medium mt-1 mb-2  ">{product.title}</p>
+                     <div className="flex gap-3 items-center">
+                        <strong className="text-zinc-700/90">
+                           {product.price.toLocaleString("pt-BR", {
+                              style: "currency",
+                              currency: "BRL",
+                           })}
+                        </strong>
+                        <button className="bg-zinc-900 p-1 rounded">
+                           <BsCartPlus size={28} color="#fff" />
+                        </button>
+                     </div>
+                  </section>
+               ))}
             </div>
          </main>
       </div>
