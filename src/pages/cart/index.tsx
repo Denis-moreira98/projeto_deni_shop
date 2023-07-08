9dd9af5be34b05bd18aa371c836360c89fc815aa
Denis Moreira <silvad281@gmail.com>
2023-07-08 16:45:46 -0300

    feat: criando pag Home e Cart

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -1,7 +1,29 @@
 export function Cart() {
    return (
-      <div>
-         <h1>PAGINA CART</h1>
+      <div className="w-full max-w-7xl mx-auto">
+         <h1 className="font-bold font-bold text-center my-4">Meu carrinho</h1>
+
+         <section className="flex items-center  justify-between border-b-2 border-gray-300">
+            <img
+               className="w-28 "
+               src="https://images.tcdn.com.br/img/img_prod/1077127/celular_xiaomi_redmi_9a_2gb_ram_dual_32gb_6_53_glacial_blue_16753_1_78f5104e6e8985618a33cdb1f312550a.jpeg"
+               alt="Foto celular"
+            />
+            <strong>Preço: R$: 1000,00</strong>
+
+            <div className="flex items-center justify-center gap-3">
+               <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+                  -
+               </button>
+               1
+               <button className="bg-slate-600 px-2 rounded text-white font-medium flex items-center justify-center">
+                  +
+               </button>
+            </div>
+            <strong className="float-right">Subtotal: R$ 1000,00</strong>
+         </section>
+
+         <p className="font-bold mt-4">Total: R$ 1000,00 </p>
       </div>
    );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,29 @@
+import { BsCartPlus } from "react-icons/bs";
+
 export function Home() {
    return (
       <div>
-         <h1>PAGINA HOME</h1>
+         <main className="w-full max-w-7xl px-4 mx-auto">
+            <h1 className="font-bold text-2xl mb-4 mt-10 text-center">
+               Produtos em alta
+            </h1>
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
+               <section className="w-full">
+                  <img
+                     className="w-full rounded-lg max-h-70 mb-b"
+                     src="https://images.tcdn.com.br/img/img_prod/1077127/celular_xiaomi_redmi_9a_2gb_ram_dual_32gb_6_53_glacial_blue_16753_1_78f5104e6e8985618a33cdb1f312550a.jpeg"
+                     alt="celular xiaomi"
+                  />
+                  <p className="font-medium mt-1 mb-2  ">Celular</p>
+                  <div className="flex gap-3 items-center">
+                     <strong className="text-zinc-700/90">R$ 1000,00</strong>
+                     <button className="bg-zinc-900 p-1 rounded">
+                        <BsCartPlus size={28} color="#fff" />
+                     </button>
+                  </div>
+               </section>
+            </div>
+         </main>
       </div>
    );
 }
